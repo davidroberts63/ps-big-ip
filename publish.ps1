0b186036431b9527646ec6fbffc7f2e320b9fc5a
David Roberts <david.roberts63@gmail.com>
2018-02-09 22:07:05 -0600

    Got the casing for the module that I wanted

--- publish.ps1
+++ publish.ps1
@@ -21,7 +21,9 @@ if($Locally) {
     New-Item -Type Directory $path -Force | Out-Null
     Register-PSRepository -Name $name -SourceLocation $path -PublishLocation $path -InstallationPolicy Trusted
 
-    Publish-Module -Path $PSScriptRoot\big-ip -Repository $name
+    # Note, the case in the folder name specified here affects how it's put into the repository.
+    # So leave it as is. Why? I don't know but it's really annoying.
+    Publish-Module -Path $PSScriptRoot\Big-Ip -Repository $name
 
     Unregister-PSRepository -Name $name
 
