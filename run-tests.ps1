f3a083fec07a6f23dc0f8f3bc70fbd49f678ed4c
David Roberts <david.roberts63@gmail.com>
2018-02-09 21:20:11 -0600

    Let dev know Pester 4 is needed for these tests

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,4 +1,11 @@
 Import-Module .\big-ip\Big-Ip.psd1
-Import-Module Pester
+Import-Module Pester -ErrorAction SilentlyContinue
+$pester = Get-Module Pester
+if(-not $pester) {
+    Write-Warning "Could not find the Pester Powershell module. 'Install-Module Pester' first."
+} elseif($pester.Version.Major -lt 4) {
+    Write-Warning "Pester 4.0 and greater is required. 'Update-Module Pester' first."
+    exit 1
+}
 
 Invoke-Pester -Path .\tests -CodeCoverage .\big-ip\functions\*
