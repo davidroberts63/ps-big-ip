13b90da8daf582399bf9c2ad7e9b47b8debae559
David Roberts <david.roberts63@gmail.com>
2018-02-15 23:43:12 -0600

    Update console output

--- run-tests.ps1
+++ run-tests.ps1
@@ -8,22 +8,16 @@ if(-not $pester) {
     exit 1
 }
 
-
 $results = Invoke-Pester -PassThru -Path $PSScriptRoot\tests `
     -OutputFile $PSScriptRoot\tests-results.xml -OutputFormat NUnitXML `
     -CodeCoverage $PSScriptRoot\big-ip\functions\* `
     -CodeCoverageOutputFile $PSScriptRoot\coverage-results.xml -CodeCoverageOutputFileFormat JaCoCo
-$results
+
+Write-Host "Generating Coveralls report"
 . .\Get-CoverallsReport.ps1
 $report = Get-CoverallsReport $results.CodeCoverage $ENV:COVERALLS_TOKEN
-#$report
 
-if($ENV:APPVEYOR_JOB_ID) {
-    Write-Host "Uploading tests results to AppVeyor"
-    $wc = New-Object 'System.Net.WebClient'
-    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($ENV:APPVEYOR_JOB_ID)", (Resolve-Path $PSScriptRoot\tests-results.xml)) | Out-Null
-}
-
+Write-Host "Uploading Coveralls report"
 Add-Type -AssemblyName System.Net.Http | Out-Null
 $formData = New-Object System.Net.Http.MultipartFormDataContent
 $content = New-Object System.Net.Http.StringContent(($report | ConvertTo-Json -Depth 10), [System.Text.Encoding]::UTF8, "application/json")
@@ -31,3 +25,9 @@ $formData.Add($content, "json_file", "coverage.json") | Out-Null
 $headers = @{ "Content-Type" = ($formData.Headers | Select Key,Value | Where Key -eq "Content-Type" | Select -ExpandProperty Value) }
 
 Invoke-RestMethod -Uri "https://coveralls.io/api/v1/jobs" -Method Post -Headers $headers -Body ($formData.ReadAsStringAsync().Result)
+
+if($ENV:APPVEYOR_JOB_ID) {
+    Write-Host "Uploading tests results to AppVeyor"
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($ENV:APPVEYOR_JOB_ID)", (Resolve-Path $PSScriptRoot\tests-results.xml)) | Out-Null
+}
